addfc2bb343422fd587af590613864a043ac2c28
许宏斌 <ckken@qq.com>
2017-12-21 01:04:03 +0800

    Add example

--- src/pages/home/home/about.js
+++ src/pages/home/home/about.js
@@ -4,10 +4,14 @@ import { Button } from 'antd'
 import styles from './about.less'
 
 export default class IndexView extends PureComponent {
+  async componentWillMount(){
+    let moment = await import('helper/moment')
+    console.log(`moment`,moment)
+  }
   render() {
     return (
       <div className={styles.page}>
-        index view
+        index view 
         <Button type="primary" size="large">
           按钮
         </Button>
